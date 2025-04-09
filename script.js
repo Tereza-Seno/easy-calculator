c0dcf2da2081a563e9fb2b5533ed8d69861f564e
Tereza Senovičová <tereza.senovicova@gmail.com>
2025-04-09 12:08:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ const section = document.querySelector (".results")
 const number1 = parseInt(prompt("Zadej číslo"))
 const number2 = parseInt(prompt("Zadej druhé čislo"))
 
-const newparagrapg1 = document.createElement ("p")
-newparagrapg1.textContent = (`Součet tvých čísel je: ${number1 + number2}`)
-section.append(newparagrapg1)
+const newparagraph1 = document.createElement ("p")
+newparagraph1.textContent = (`Součet tvých čísel je: ${number1 + number2}`)
+section.append(newparagraph1)
 
-const newparagrapg2 = document.createElement ("p")
-newparagrapg2.textContent = (`Rozdíl tvých čísel je: ${number1 - number2}`)
-section.append(newparagrapg2)
+const newparagraph2 = document.createElement ("p")
+newparagraph2.textContent = (`Rozdíl tvých čísel je: ${number1 - number2}`)
+section.append(newparagraph2)
 
-const newparagrapg3 = document.createElement ("p")
-newparagrapg3.textContent = (`Násobek tvých čísel je: ${number1 * number2}`)
-section.append(newparagrapg3)
+const newparagraph3 = document.createElement ("p")
+newparagraph3.textContent = (`Násobek tvých čísel je: ${number1 * number2}`)
+section.append(newparagraph3)
 
-const newparagrapg4 = document.createElement ("p")
-newparagrapg4.textContent = (`Podíl tvých čísel je: ${number1 / number2}`)
-section.append(newparagrapg4)
+const newparagraph4 = document.createElement ("p")
+newparagraph4.textContent = (`Podíl tvých čísel je: ${number1 / number2}`)
+section.append(newparagraph4)
+
 
 
