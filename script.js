d2a0cacc069283ab9449b9e6dcef426977245d2e
Tereza Senovičová <tereza.senovicova@gmail.com>
2025-05-25 20:33:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-const section = document.querySelector (".results")
+const results = document.querySelector(".results");
 
-const number1 = parseInt(prompt("Zadej číslo"))
-const number2 = parseInt(prompt("Zadej druhé čislo"))
+const number1 = prompt("Zadej číslo (můžeš použít i desetinnou čárku).");
+const number2 = prompt("Zadej druhé číslo (můžeš použít i desetinnou čárku).");
 
-const newparagraph1 = document.createElement ("p")
-newparagraph1.textContent = (`Součet tvých čísel je: ${number1 + number2}`)
-section.append(newparagraph1)
+const numberComma1 = parseFloat(number1.replace(",", "."));
+const numberComma2 = parseFloat(number2.replace(",", "."));
 
-const newparagraph2 = document.createElement ("p")
-newparagraph2.textContent = (`Rozdíl tvých čísel je: ${number1 - number2}`)
-section.append(newparagraph2)
+const addition = numberComma1 + numberComma2;
+const subtraction = numberComma1 - numberComma2;
+const multiplication = numberComma1 * numberComma2;
+const division = numberComma1 / numberComma2;
 
-const newparagraph3 = document.createElement ("p")
-newparagraph3.textContent = (`Násobek tvých čísel je: ${number1 * number2}`)
-section.append(newparagraph3)
+console.log(addition, subtraction, multiplication, division);
 
-const newparagraph4 = document.createElement ("p")
-newparagraph4.textContent = (`Podíl tvých čísel je: ${number1 / number2}`)
-section.append(newparagraph4)
+const heading = document.createElement("h1");
+const paragraph1 = document.createElement("p");
+const paragraph2 = document.createElement("p");
+const paragraph3 = document.createElement("p");
+const paragraph4 = document.createElement("p");
 
+heading.textContent = "Jednoduchá kalkulačka (JavaScript)";
+paragraph1.textContent = `Součet tvých čísel je: ${addition}`;
+paragraph2.textContent = `Rozdíl tvých čísel je: ${subtraction}`;
+paragraph3.textContent = `Součin tvých čísel je: ${multiplication}`;
+paragraph4.textContent = `Podíl tvých čísel je: ${division}`;
 
-
+results.append(heading, paragraph1, paragraph2, paragraph3, paragraph4);
